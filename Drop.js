d10bb78adaee021ee93a22e77302fe675ccef342
Losenges2040 <73734992+Losenges2040@users.noreply.github.com>
2021-05-04 17:56:53 +0530

    Update Drop.js

--- Drop.js
+++ Drop.js
@@ -3,18 +3,22 @@ class createDrops{
         var options = {
             airFriction :0.1,
             Friction:0.5
+            
         }
 
+        this.image = loadImage("bat/ok654.png");
         this.rain = Bodies.circle(x,y,5,options)
+        World.add(world, this.rain)
     }
     update(){
         if(this.rain.position.y > height){
             Matter.Body.setPosition(this.rain, {x:random(0,400)},{y:random(0,400)})
         }
     }
-
     display(){
-        var pos = this.rain.position;
         imageMode(CENTER);
+        image(this.image,this.rain.position.x,this.rain.position.y);
+
     }
 }
+
